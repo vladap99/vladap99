9481387e9e7a8018d2995edb121eb08f29006a98
gabiap96 <apostol.gabriel90@gmail.com>
2021-02-11 20:55:13 +0200

    deploy

---
+++ js/about.81962e53.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{abec:function(n,e,t){"use strict";t.r(e);var a=t("7a23"),c={class:"logos"},o=Object(a["g"])("h1",null,"This is an about page",-1);function s(n,e){return Object(a["p"])(),Object(a["d"])("div",c,[o])}const u={};u.render=s;e["default"]=u}}]);
+//# sourceMappingURL=about.81962e53.js.map
